4eedbbaad7c5b10df1a6002b4e69b17f8f3e9459
Rushikesh Acharya <v-racharya@microsoft.com>
2023-09-17 21:41:00 +0530

    Lab 8: Reusable component

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -4,6 +4,7 @@
 
 import React from "react";
 import { Project } from "./Project";
+import ProjectCard from "./ProjectCard";
 
 interface ProjectListProps {
   projects: Project[];
@@ -14,16 +15,7 @@ function ProjectList({ projects }: ProjectListProps) {
     <div className="row">
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
-          <div className="card">
-            <img src={project.imageUrl} alt={project.name} />
-            <section className="section dark">
-              <h5 className="strong">
-                <strong>{project.name}</strong>
-              </h5>
-              <p>{project.description}</p>
-              <p>Budget: {project.budget.toLocaleString()}</p>
-            </section>
-          </div>
+            <ProjectCard project={project}/>
         </div>
       ))}
     </div>
